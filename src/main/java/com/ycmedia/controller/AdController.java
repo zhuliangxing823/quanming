57cc30c4b897bdb5e837640ee0f974766cfd765f
朱良兴 <zhuliangxing@ycmedia.cn>
2016-08-26 09:56:10 +0800

    添加模板

--- src/main/java/com/ycmedia/controller/AdController.java
+++ src/main/java/com/ycmedia/controller/AdController.java
@@ -35,6 +35,8 @@ public class AdController {
 	private AdService adservice;
 	
 	Creative creative = new Creative();
+	
+	CreativeTpl tpl = new CreativeTpl();
 
 	/**
 	 * 查询广告列表
@@ -78,7 +80,7 @@ public class AdController {
 		return new ModelAndView("creativetpl-list", "list", list);
 	}
 	
-	/**
+	/**tpl-add
 	 * 跳转到编辑广告
 	 * @return
 	 */
@@ -88,14 +90,30 @@ public class AdController {
 		return new ModelAndView("product-add" ,"creative",creative );
 	}
 	
+	/**
+	 * 
+	 * 跳转到编辑广告
+	 * @return
+	 */
+	@RequestMapping(value = "/tpl-add")
+	public ModelAndView  toTplAdd(@ModelAttribute(value="tpl")CreativeTpl tpl){
+		return new ModelAndView("tpl-add" );
+	}
+	
+	
+	/**
+	 * 审核内容修改
+	 * @param creative
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/editAd")
 	public ModelAndView  editAdReason(@ModelAttribute(value="creative")Creative creative,Model model ){
-		
 		try {
 			adservice.updateCreative(creative);
-			model.addAttribute("code", "200");
+			model.addAttribute("success", true);
 		} catch (Exception e) {
-			model.addAttribute("code", "500");
+			model.addAttribute("success", false);
 			
 		}
 		return  new ModelAndView("redirect:/product-list");
